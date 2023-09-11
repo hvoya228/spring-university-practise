dd577b5a39294f023c4b57b8c18e38116c244b8d
Chugaister <bogucukij@gmail.com>
2023-09-11 13:48:41 +0300

    laba19 added

---
+++ laba19/src/test/java/org/o7planning/sbgroovy/Laba19ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.o7planning.sbgroovy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laba19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
