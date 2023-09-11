270fd046fd3571d071e681a38303f94cb5a4aa26
Chugaister <bogucukij@gmail.com>
2023-09-11 14:20:56 +0300

    laba20 added

---
+++ laba20/src/main/java/org/o7planning/sbmustache/Laba20Application.java
@@ -0,0 +1,13 @@
+package org.o7planning.sbmustache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laba20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laba20Application.class, args);
+	}
+
+}
