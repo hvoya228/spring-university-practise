65a508185cf3c67c6fde9d0da04c01dd255bbc83
Chugaister <bogucukij@gmail.com>
2023-11-26 01:25:44 +0200

    laba28-29 added

---
+++ laba29/src/main/java/org/o7planning/sbsecurity/Laba29Application.java
@@ -0,0 +1,13 @@
+package org.o7planning.sbsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laba29Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laba29Application.class, args);
+	}
+
+}
