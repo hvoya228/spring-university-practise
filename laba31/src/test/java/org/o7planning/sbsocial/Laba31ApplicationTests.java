21256e606bb5e7cd605c61aca8764b347be8df3f
Chugaister <bogucukij@gmail.com>
2023-11-26 02:48:13 +0200

    laba31 added

---
+++ laba31/src/test/java/org/o7planning/sbsocial/Laba31ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.o7planning.sbsocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laba31ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
