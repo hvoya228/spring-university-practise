87016686e1cb4ac060faa0dea4512110d347c183
Chugaister <bogucukij@gmail.com>
2023-11-26 02:04:43 +0200

    laba30 added

---
+++ laba30/src/main/java/org/o7planning/sbformvalidation/Laba30Application.java
@@ -0,0 +1,13 @@
+package org.o7planning.sbformvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laba30Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laba30Application.class, args);
+	}
+
+}
