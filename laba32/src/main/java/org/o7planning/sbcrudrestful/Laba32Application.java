576ca074a29284d89b746c026e3d5509c3d854c3
Chugaister <bogucukij@gmail.com>
2023-11-26 03:00:13 +0200

    laba32 added

---
+++ laba32/src/main/java/org/o7planning/sbcrudrestful/Laba32Application.java
@@ -0,0 +1,13 @@
+package org.o7planning.sbcrudrestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laba32Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laba32Application.class, args);
+	}
+
+}
